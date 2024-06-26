0d68a3d085051c36a7d119fe7665fd30261be8a6
R.Benatti <162356647+rosanabenatti@users.noreply.github.com>
2024-06-25 21:07:50 -0300

    Added basic JS, handling and typewriter.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Caprese salad",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
